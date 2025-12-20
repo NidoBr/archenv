7cfaae691b900bc3add27e27a907caced78e247c
NidoBr <coqecoisa@gmail.com>
2025-12-19 21:21:14 -0300

    Main files:
    arch_buildenv - generates the environment
    archenv - starts/stops the environment correctly
    init_config.sh - configures the environment

---
+++ init_config.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#
+# NidoBr <nidobrcontato@gmail.com>
+#
+
+# Configura idioma e hora
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
+echo 'LANG=pt_BR.UTF-8' > /etc/locale.conf
+locale-gen
+echo 'KEYMAP=br-abnt2' > /etc/vconsole.conf
+
+# Configura usuario
+useradd -mU ubuild
+echo "ubuild ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+# Iniciar as chaves do pacman
+pacman-key --init
+pacman-key --populate archlinux
+
+# Script para iniciar o TMPFS para compilação em RAM
+echo "#!/usr/bin/env bash
+mount --mkdir -t tmpfs -o size=40G,noatime,nodev,nosuid tmpfs /var/makepkg
+exit 0
+" > /usr/local/sbin/start_tmpfs
+chmod +x /usr/local/sbin/start_tmpfs
+
+exit 0
